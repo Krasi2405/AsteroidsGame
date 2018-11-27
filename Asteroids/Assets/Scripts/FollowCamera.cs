b5fb4315f9a8d7c90dd7988881bc3fc3d199cf5b
Krasimir Georgiev <krasi2405@gmail.com>
2018-11-27 19:16:37 +0200

    + Add 2 simple movement modes with new animations + Import Post Processing Stack + Import CrossPlatformInput + Import ParticleSystems

---
+++ Asteroids/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerController player;
+
+    private Vector3 offset;
+
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+        offset = transform.position - player.transform.position;
+	}
+	
+
+	void LateUpdate () {
+        // transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, 0.2f);
+        transform.position = player.transform.position + offset;
+	}
+}
