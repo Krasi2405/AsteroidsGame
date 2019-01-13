86d3a74ec4bae609b286f10d4626e5460d29a0ab
Krasimir Georgiev <krasi2405@gmail.com>
2019-01-13 18:07:46 +0200

    Powerups.

---
+++ Asteroids/Assets/PowerUps/HealthPowerup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerup : Powerup
+{
+    [SerializeField]
+    private float flatHealthPoints = 20f;
+
+    protected override void ActivatePowerup(Spaceship spaceship)
+    {
+        float hp = flatHealthPoints;
+        spaceship.AddHitpoints(hp);
+    }
+}
---
+++ Asteroids/Assets/PowerUps/Powerup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class Powerup : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObj = other.gameObject;
+        Debug.Log(gameObj.name + " enter powerup!");
+        if (gameObj.GetComponent<PlayerController>())
+        {
+            ActivatePowerup(gameObj.GetComponent<Spaceship>());
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract void ActivatePowerup(Spaceship spaceship);
+}
+
